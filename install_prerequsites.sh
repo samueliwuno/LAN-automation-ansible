89381da3e62173651e379981e555488a78debebf
Samuel Iwuno <38007169+samueliwuno@users.noreply.github.com>
2021-05-03 11:32:24 -0400

    Update install_prerequsites.sh

--- install_prerequsites.sh
+++ install_prerequsites.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "installing python3 prerequisites"
+echo "installing prerequisites"
+sudo add-apt-repository ppa:gns3/ppa
+sudo apt update
 sudo apt install -y python3 python3-pip
 echo "installing python3 prerequisites"
 python3 install pip
@@ -9,13 +11,19 @@ python3 install pip
 echo "Installing ansible via pip3"
 pip3 install ansible
 
+
+
 echo "installing ansible-galaxy collection packages for EXOS and Fortinet" 
 ansible-galaxy collection install community.network
 ansible-galaxy collection install extreme.exos
 ansible-galaxy collection install -f fortinet.fortios:1.0.11
+
 echo "setting up ssh-agent for ansible"
 ssh-agent bash
 ssh-add ~/.ssh/known_hosts
 export ANSIBLE_HOST_KEY_CHECKING=False
+
+echo "installing GNS3"                               
+sudo apt install gns3 gns3-gui gns3-server 
 
 exec bash
